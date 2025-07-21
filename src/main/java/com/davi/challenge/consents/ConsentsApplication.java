950698744d62a4b0d3386a7b71839125a93a6858
Davi Lopes <34341619+davilopess@users.noreply.github.com>
2025-07-21 20:34:08 -0300

    chore: Estrutura inicial do projeto com SpringBoot

---
+++ src/main/java/com/davi/challenge/consents/ConsentsApplication.java
@@ -0,0 +1,13 @@
+package com.davi.challenge.consents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davi/challenge/consents/ConsentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.challenge.consents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
